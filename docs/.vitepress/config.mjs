7ad7d8e75d929a94254b7cc9bd7bfbb1cc1f893c
hyebinpark <hoonywhitesocks@gmail.com>
2025-09-18 13:29:40 +0900

    docs(database): add SQL introduction

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -479,6 +479,7 @@ export default defineConfig({
           items: [
             { text: "Database", link: "/database/db00" },
             { text: "SQL", link: "/database/db01" },
+            { text: "SQL 데이터 타입", link: "/database/db02" },
           ],
         },
       ],
